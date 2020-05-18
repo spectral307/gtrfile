87fe5e9fbc60fc770b66867ab3a1a3d63a1108f2
Anton A. Umarov <Umarov@gt.ru>
2020-05-18 16:27:04 +0300

    2020-05-18

--- gtrfile.py
+++ gtrfile.py
@@ -1,29 +1,35 @@
+from datetime import timedelta
 import numpy as np
 import xml.etree.ElementTree as ET
-from datetime import timedelta
 
 
 class GtrFile:
 
-    __file = None
-    __bin_section_start = None
+    #############################################
+    # Member Variables #
+    #############################################
     __bin_section_end = None
     __bin_section_size = None
-    __samples_number_per_input = None
-    __record_duration = None
-    __inputs_number = None
+    __bin_section_start = None
+    __samples_dtype = None
+    __file = None
     __header = {
-        "encoding": "cp1251",
         "device": None,
-        "rate": None,
-        "time": None,
-        "text": None,
+        "encoding": "cp1251",
         "formatted_text": None,
-        "inputs": []
+        "inputs": [],
+        "rate": None,
+        "text": None,
+        "time": None
     }
-    __dtype = None
+    __inputs_number = None
+    __record_duration = None
+    __samples_number_per_input = None
     __size_of_float = 4
 
+    #############################################
+    # Magic Methods #
+    #############################################
     def __init__(self, path: str):
         self.__file = open(path, "rb")
         self.__file.seek(4, 0)
@@ -40,11 +46,63 @@ class GtrFile:
         self.__samples_number_per_input = self.__bin_section_size \
             // self.__inputs_number // self.__size_of_float
 
-        obj = []
-        for i, inp in enumerate(self.__header["inputs"]):
-            obj.append((f"{inp['name']}", np.float32))
-        self.__dtype = np.dtype(obj)
+        self.__infer_samples_dtype()
 
+    def __str__(self):
+        ind = "  "
+        inputs_str = "["
+        for i, inp in enumerate(self.__header["inputs"]):
+            inputs_str += f"{',' if i != 0 else ''}\n{ind}{ind}{ind}{{"
+            inputs_str += f"\n{ind}{ind}{ind}{ind}n: {inp['n']}"
+            inputs_str += f"\n{ind}{ind}{ind}{ind}name: {inp['name']},"
+            inputs_str += f"\n{ind}{ind}{ind}{ind}iepe: {inp['iepe']},"
+            inputs_str += f"\n{ind}{ind}{ind}{ind}coupling: {inp['coupling']},"
+            inputs_str += f"\n{ind}{ind}{ind}{ind}sensitivity: {inp['sensitivity']},"
+            inputs_str += f"\n{ind}{ind}{ind}{ind}unit: {inp['unit']},"
+            inputs_str += f"\n{ind}{ind}{ind}{ind}offset: {inp['offset']},"
+            inputs_str += f"\n{ind}{ind}{ind}}}"
+        inputs_str += f"\n{ind}{ind}]"
+        return ("<GtrFile instance:"
+                + f"\n{ind}samples_number_per_input: {self.__samples_number_per_input},"
+                + f"\n{ind}bin_section_start: {self.__bin_section_start},"
+                + f"\n{ind}bin_section_end: {self.__bin_section_end},"
+                + f"\n{ind}bin_section_size: {self.__bin_section_size},"
+                + f"\n{ind}record_duration: {self.__record_duration},"
+                + f"\n{ind}inputs_number: {self.__inputs_number},"
+                + f"\n{ind}dtype: {self.__samples_dtype},"
+                + f"\n{ind}header: {{"
+                + f"\n{ind}{ind}encoding: {self.__header['encoding']},"
+                + f"\n{ind}{ind}device: {self.__header['device']},"
+                + f"\n{ind}{ind}rate: {self.__header['rate']},"
+                + f"\n{ind}{ind}time: {self.__header['time']},"
+                + f"\n{ind}{ind}inputs: {inputs_str}"
+                + "\n>")
+
+    def __del__(self):
+        if not self.closed:
+            self.close()
+
+    #############################################
+    # Public Methods #
+    #############################################
+    def get_samples(self, start: int, size: int):
+        self.__seek_sample(start)
+
+        if size > self.__get_remainder_size():
+            raise Exception("Size is greater than remainder size")
+
+        s = np.fromfile(self.__file, dtype=self.dtype, count=size)
+        t = self.__calc_time_vector_in_seconds(start, size)
+
+        return (t, s)
+
+    def close(self):
+        if not self.closed:
+            self.__file.close()
+
+    #############################################
+    # Properties #
+    #############################################
     @property
     def closed(self):
         return self.__file.closed
@@ -59,8 +117,11 @@ class GtrFile:
 
     @property
     def dtype(self):
-        return self.__dtype
+        return self.__samples_dtype
 
+    #############################################
+    # Private Methods #
+    #############################################
     def __calculate_bin_section_parameters(self):
         self.__bin_section_start = self.__file.tell()
         self.__file.seek(0, 2)
@@ -97,13 +158,19 @@ class GtrFile:
                 "coupling": inp.attrib["coupling"]
             })
 
+    def __infer_samples_dtype(self):
+        obj = []
+        for i, inp in enumerate(self.__header["inputs"]):
+            obj.append((f"{inp['name']}", np.float32))
+        self.__samples_dtype = np.dtype(obj)
+
     def __seek_sample(self, offset):
         if offset < 0 or offset > self.__samples_number_per_input:
             raise Exception("Wrong offset value")
 
         self.__size_of_float = 4
-        bin_offset = self.__bin_section_start + \
-            offset * self.__inputs_number * self.__size_of_float
+        bin_offset = self.__bin_section_start + offset * \
+            self.__inputs_number * self.__size_of_float
 
         if bin_offset == self.__file.tell():
             return
@@ -115,52 +182,6 @@ class GtrFile:
         return ((self.__bin_section_end - self.__file.tell())
                 // self.__inputs_number // self.__size_of_float)
 
-    def get_samples(self, start, arr: np.ndarray):
-        self.__seek_sample(start)
-
-        if arr.dtype != self.dtype:
-            raise Exception("Wrong array dtype")
-
-        if arr.size > self.__get_remainder_size():
-            raise Exception("Array too large for the data remainder")
-
-        np.copyto(arr, np.fromfile(
-            self.__file, dtype=self.dtype, count=arr.size))
-
-    def close(self):
-        if not self.closed:
-            self.__file.close()
-
-    def __str__(self):
-        ind = "  "
-        inputs_str = "["
-        for i, inp in enumerate(self.__header["inputs"]):
-            inputs_str += f"{',' if i != 0 else ''}\n{ind}{ind}{ind}{{"
-            inputs_str += f"\n{ind}{ind}{ind}{ind}n: {inp['n']}"
-            inputs_str += f"\n{ind}{ind}{ind}{ind}name: {inp['name']},"
-            inputs_str += f"\n{ind}{ind}{ind}{ind}iepe: {inp['iepe']},"
-            inputs_str += f"\n{ind}{ind}{ind}{ind}coupling: {inp['coupling']},"
-            inputs_str += f"\n{ind}{ind}{ind}{ind}sensitivity: {inp['sensitivity']},"
-            inputs_str += f"\n{ind}{ind}{ind}{ind}unit: {inp['unit']},"
-            inputs_str += f"\n{ind}{ind}{ind}{ind}offset: {inp['offset']},"
-            inputs_str += f"\n{ind}{ind}{ind}}}"
-        inputs_str += f"\n{ind}{ind}]"
-        return ("<GtrFile instance:"
-                + f"\n{ind}samples_number_per_input: {self.__samples_number_per_input},"
-                + f"\n{ind}bin_section_start: {self.__bin_section_start},"
-                + f"\n{ind}bin_section_end: {self.__bin_section_end},"
-                + f"\n{ind}bin_section_size: {self.__bin_section_size},"
-                + f"\n{ind}record_duration: {self.__record_duration},"
-                + f"\n{ind}inputs_number: {self.__inputs_number},"
-                + f"\n{ind}dtype: {self.__dtype},"
-                + f"\n{ind}header: {{"
-                + f"\n{ind}{ind}encoding: {self.__header['encoding']},"
-                + f"\n{ind}{ind}device: {self.__header['device']},"
-                + f"\n{ind}{ind}rate: {self.__header['rate']},"
-                + f"\n{ind}{ind}time: {self.__header['time']},"
-                + f"\n{ind}{ind}inputs: {inputs_str}"
-                + "\n>")
-
-    def __del__(self):
-        if not self.closed:
-            self.close()
+    def __calc_time_vector_in_seconds(self, start, size):
+        sampling_interval = 1 / self.header["rate"]
+        return sampling_interval * np.arange(start, start+size)
--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-from pandas import to_datetime
 from gtrfile import GtrFile
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
 
-def calc_time_in_seconds(duration, rate):
-    return np.arange(duration, step=1/rate)
-
-
-def calc_time_index(duration, rate):
-    return to_datetime(calc_time_in_seconds(duration, rate),
-                       unit="s")
-
-
 def main():
-    path = "test_record.gtr"
+    path = "records/test.gtr"
 
     if len(sys.argv) > 1:
         path = sys.argv[1]
@@ -24,10 +14,7 @@ def main():
 
     print(gtr)
 
-    s = np.empty(gtr.samples_number_per_input, dtype=gtr.dtype)
-    gtr.get_samples(0, s)
-
-    t = calc_time_index(gtr.header["time"], gtr.header["rate"])
+    t, s = gtr.get_samples(2001, 1199)
 
     ax = plt.subplot()
     plt.plot(t, s[gtr.header["inputs"][0]["name"]],
